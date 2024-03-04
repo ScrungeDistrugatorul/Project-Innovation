1c7480689f2332cae02588fa1c787eed94f3a0b6
Mordie2 <118892288+Mordie2@users.noreply.github.com>
2024-03-04 16:44:02 +0100

    Character animations
    Characters and lobby collisions

--- Project-Innovation/Assets/Scripts/Character/CharacterMovement.cs
+++ Project-Innovation/Assets/Scripts/Character/CharacterMovement.cs
@@ -11,11 +11,13 @@ public class CharacterMovement : MonoBehaviour
     private float gravityValue = -9.81f;
 
     private Vector3 move;
+    public Animator animator;
     
         private void Awake()
         {
             controller = GetComponent<CharacterController>();
-        }
+            animator = GetComponent<Animator>();
+    }
     
         void Update()
         {
@@ -23,28 +25,36 @@ public class CharacterMovement : MonoBehaviour
             if (groundedPlayer && playerVelocity.y < 0)
             {
                 playerVelocity.y = 0f;
-            }
-    
-            controller.Move(move * Time.deltaTime * playerSpeed);
+                
+
+
+        }
+        
+        controller.Move(move * Time.deltaTime * playerSpeed);
     
             if (move != Vector3.zero)
             {
                 gameObject.transform.forward = move;
-            }
-    
-            // // Changes the height position of the player..
-            // if (Input.GetButtonDown("Jump") && groundedPlayer)
-            // {
-            //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            // }
-    
-            playerVelocity.y += gravityValue * Time.deltaTime;
-            controller.Move(playerVelocity * Time.deltaTime);
+            
+            animator.SetTrigger("walk");
         }
+
+        // // Changes the height position of the player..
+        // if (Input.GetButtonDown("Jump") && groundedPlayer)
+        // {
+        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        // }
+        
+        playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+        //UnityEngine.Debug.Log(animator.GetFloat("Vertical"));
+            
+    }
 
         public void OnMove(InputAction.CallbackContext context)
         {
-            Vector2 movement = context.ReadValue<Vector2>();
+        animator.ResetTrigger("walk");
+        Vector2 movement = context.ReadValue<Vector2>();
             move = new Vector3(-movement.x, 0, -movement.y);
         }
 }
